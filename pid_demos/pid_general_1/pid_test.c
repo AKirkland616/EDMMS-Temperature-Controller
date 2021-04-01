46ee95fcf7727527104aa7e1909da345b7a55b99
JeremyMEvans <jeremy-m-evans@outlook.com>
2021-04-01 18:59:23 -0400

    updated updaetTestMeasurement comment description in pid_test.c.

--- pid_demos/pid_general_1/pid_test.c
+++ pid_demos/pid_general_1/pid_test.c
@@ -15,11 +15,17 @@
 #include "pid.h"
 
 /**
- * Simulates a retrieval of a measurement following
+ * Emulates a plant process mapping the output from the simulated PID controller
+ * (the control variable) to the resulting measurement taken from the controlled
+ * system (the process variable).
+ * 
+ * @param pidOutput The output of the PID controller at the previous moment in time.
+ * @param measurement The measurement of the system taken at the previous moment in time.
+ * 
+ * @returns The measurement of the system taken at the current moment in time.
  */
 float updateTestMeasurement(float pidOutput, float measurement)
 {
-    // static float output = 0.0f;
     static const float alpha = 0.02f;
 
     measurement = (TEST_SAMPLE_TIME_S * pidOutput + measurement) / (1.0f + alpha * TEST_SAMPLE_TIME_S);
