abdc6989f0fcf9627937f41b880cd456adb2cf62
JeremyMEvans <jeremy-m-evans@outlook.com>
2021-04-27 21:36:20 -0400

    Added PID Overview doc and demo code (contained in pid_demos/) to main repo.

---
+++ pid_demos/pid_general_1/pid_test_constants.h
@@ -0,0 +1,22 @@
+// Program:     Pid_General_1
+// File:        pid_test_constants.h
+// Author:      Jeremy Evans
+//
+// Description: Constants used when testing this implementation
+//              of the PID algorithm.
+//
+// ==============================================================================
+
+#define TEST_PROPORTIONAL_GAIN 2.0f
+#define TEST_INTEGRAL_GAIN 0.5f
+#define TEST_DERIVATIVE_GAIN 0.5f
+
+#define TEST_LPF_CONSTANT 0.02f
+
+#define TEST_OUTPUT_MIN -10.0f
+#define TEST_OUTPUT_MAX 10.0f
+
+#define TEST_LIM_MIN_INT -5.0f
+#define TEST_LIM_Max_INT 5.0f
+
+#define TEST_SAMPLE_TIME_S 0.01f
